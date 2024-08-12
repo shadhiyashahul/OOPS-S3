d9de2cf0aa42ab30645a6f66bc040cc777539943
shadhiyashahul <shadhiyashahul8@gmail.com>
2024-08-12 10:40:57 +0530

    Add files via upload

---
+++ LAB/frequency.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class frequency
+{
+	public static void main(String args[])
+		{  
+		   int count=0;
+			Scanner S=new Scanner(System.in);
+			System.out.println("enter the string");
+			String sr;
+			sr=S.nextLine();
+			sr.toLowerCase();
+			System.out.println("enter the character to check");
+		   char c= S.next().charAt(0);
+		   int len=sr.length();
+		   for(int i=0;i<len;i++)
+		   {
+		   	if(sr.charAt(i)== c)
+		   	{
+		   		count++;
+		   	}
+		   }
+		   System.out.println("the frequency of character is"+count);
+		   S.close();
+		   }
+}
