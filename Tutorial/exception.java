ffac0e4df0f43cefbe08fc7fbf7a8b5584b4b004
shadhiyashahul <shadhiyashahul8@gmail.com>
2024-10-05 11:20:54 +0530

    Add files via upload

---
+++ Tutorial/exception.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class exception
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			int[] num={1,2,3};
+		}
+		catch (Exception e)
+		{
+			System.out.println("error occured in program");
+		}
+		finally 
+		{
+			System.out.println("Try ans Catch excecuted successfully");
+		}
+	}
+}
